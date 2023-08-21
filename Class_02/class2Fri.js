918689b43c8df4ea0257082697d75830c0081100
Farhan-Zafar-Aziz <139948637+Farhan-Zafar-Aziz@users.noreply.github.com>
2023-08-21 23:31:37 +0500

    Typescript Fundamentals

---
+++ Class_02/class2Fri.js
@@ -0,0 +1,61 @@
+//1.Variables      
+var piaic = "Penaverse Bokks";
+console.log(piaic);
+var piaPak = "Air Force";
+console.log(piaPak);
+//var myMessage = "Hello Dear Sir";
+var myMessage = "Hello Dear"; //here you give value to variable name and
+myMessage = "Hello Sir"; //here you changed the variable name value now if you console this variable name then ansr will be 2nd value  
+console.log(myMessage);
+//const myMessage = "Hello Aziz";
+//console.log(myMessage);
+//2.Primitive Data Types
+//These are declaration concept further use as print data
+// let msg = "Hello World";//string  
+//  let nmbrs = 123; //number
+//  let corrections = true;//boolean
+//  let myMessage:string ="Student"; 
+// let myAge: number = 19;
+// let isStudent: boolean = true;
+//Undefined
+var firstName;
+console.log(firstName); //undefined
+//Null
+var empty = null;
+console.log(empty);
+//Concatinations and Backticks
+var first_Name = "Sadiq";
+var age = 22;
+var mesge = "Hello my name is" + first_Name + "and my age is" + age;
+var mesage = "Hello my name is ".concat(first_Name, " and my age is ").concat(age);
+console.log(mesge); //Concatination
+console.log(mesage); //Backticks
+//Checking Type of Variables
+var three = "3";
+console.log(typeof ("3")); //string bcz nmbr b/w inverted commas express this value is string
+var nmbr = 20;
+console.log(typeof nmbr); //Number
+var isSelect = false;
+console.log(isSelect); //Boolean
+//Examples related to topic
+//variable names are some crazy you can use as your mined 
+var abc = 2;
+var bc = 4;
+var cd = Math.pow(abc, bc);
+console.log(cd);
+var g = true;
+console.log(typeof g);
+var atoz = "Hello";
+console.log(typeof atoz);
+var b = 22;
+console.log(typeof b);
+var totalMarks = 550;
+var obtainMarks = 450;
+var result = (totalMarks / obtainMarks) * 100;
+console.log(result);
+var a = 10;
+// a = a + 5
+//a+=5
+console.log(a);
+a -= 5;
+console.log(a);
---
+++ Class_02/class2Fri.ts
@@ -0,0 +1,88 @@
+
+//1.Variables      
+
+var piaic = "Penaverse Bokks";
+console.log(piaic);
+
+var piaPak = "Air Force";
+console.log(piaPak);
+
+//change variable value after declaration
+
+let myMessage = "Hello Dear";//here you give value to variable name and
+myMessage="Hello Sir"//here you changed the variable name value now if you console this variable name then ansr will be 2nd value  
+console.log(myMessage);//Hello Sir //it mean you can change variable value after declaration 
+
+//const myMessage = "Hello Aziz";
+
+//console.log(myMessage);
+
+//2.Primitive Data Types
+//These are declaration concept further use as print data
+
+// let msg = "Hello World";//string  
+//  let nmbrs = 123; //number
+//  let corrections = true;//boolean
+
+//  let myMessage:string ="Student"; 
+// let myAge: number = 19;
+// let isStudent: boolean = true;
+
+//Undefined
+
+let firstName
+console.log(firstName)//undefined
+
+//Null
+
+let empty = null;
+console.log(empty)
+
+//Concatinations and Backticks
+
+let first_Name = "Sadiq";
+let age = 22;
+let mesge = "Hello my name is" + first_Name + "and my age is" + age;
+let mesage = `Hello my name is ${first_Name} and my age is ${age}`;
+console.log(mesge);//Concatination
+console.log(mesage);//Backticks
+
+//Checking Type of Variables
+
+let three = "3";
+console.log(typeof("3"));//string bcz nmbr b/w inverted commas express this value is string
+let nmbr = 20
+console.log(typeof nmbr);//Number
+let isSelect = false;
+console.log(isSelect);//Boolean
+
+//Examples related to topic
+//variable names are some crazy you can use as your mined 
+
+let abc = 2
+let bc = 4
+let cd = abc ** bc;
+console.log(cd);
+
+let g =true
+console.log(typeof g)
+
+let atoz = "Hello"
+console.log(typeof atoz)
+
+let b = 22
+console.log(typeof b);
+
+let totalMarks = 550;
+let obtainMarks = 450;
+let result=(totalMarks/obtainMarks)*100;
+console.log(result)
+
+let a =10
+// a = a + 5
+//a+=5
+console.log(a)
+a-=5
+console.log(a)
+
+
